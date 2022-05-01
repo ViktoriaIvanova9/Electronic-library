0614c40c4d9cce3eef172e7cf51c7528d3d16b22
ViktoriaIvanova9 <92144440+ViktoriaIvanova9@users.noreply.github.com>
2022-05-01 13:17:05 +0300

    Add files via upload

--- Book.h
+++ Book.h
@@ -1,22 +1,28 @@
 #ifndef __BOOK_H
 #define __BOOK_H
 
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
 class Book
 {
 private:
-    char* nameAuthor;
-    char* bookTitle;
-    char* nameTextFile;
-    char* shortDescription;
+    char *nameAuthor;
+    char *bookTitle;
+    std::fstream nameTextFile;
+    char *shortDescription;
     float rating;
-    char* ISBN;
+    char *ISBN;
 
     void copyFrom();
     void clear();
+
 public:
     Book();
-    Book(const Book& other);
-    Book& operator=(const Book& other);
+    Book(const char *nameAuthor, const char *bookTitle, std::fstream nameTextFile, const char *shortDescription, float rating, const char *ISBN);
+    Book(const Book &other);
+    Book &operator=(const Book &other);
     ~Book();
 };
 
